7cb8a0b26f02d135f83565868f8ebe10cd2500fd
jotagep <jotagmzn@gmail.com>
2021-08-22 01:38:07 +0200

    Add contract testing

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,56 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    await market.deployed();
+    const marketAddress = market.address;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress);
+    await nft.deployed()
+    const nftContractAddress = nft.address
+    
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether');
+    
+    await nft.createToken("https://tokenUrl.com")
+    await nft.createToken("https://tokenUrl2.com")
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const txOptions = { value: listingPrice };
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, txOptions)
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, txOptions)
+
+    const[_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice })
+
+    const items = await market.fetchMarketItems()
+
+    const itemsParsed = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      return {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+    }))
+    
+    const expected = [{
+      price: '100000000000000000000',
+      tokenId: '2',
+      seller: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      owner: '0x0000000000000000000000000000000000000000',
+      tokenUri: 'https://tokenUrl2.com'
+    }]
+
+    expect(itemsParsed).to.deep.equal(expected)
   });
 });
