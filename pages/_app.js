f045a0053039ef697d24c027404e4b4d05804b9e
jotagep <jotagmzn@gmail.com>
2021-08-22 02:00:04 +0200

    Add layout nav to frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import Link from 'next/link'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="flex justify-between border-b p-6">
+        <h1 className="text-4xl font-bold">NFT Marketplace</h1>
+        <div className="flex items-center">     
+          <Link href="/">
+            <a className="mr-4 text-pink-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">
+              Sell Digital Asset
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">
+              My Digital Assets
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">
+              Creator Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
